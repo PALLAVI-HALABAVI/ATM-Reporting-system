2f460dc38d4e549a32a6ab8a37ba51dd7e6c59b3
Pallavi R H <138222734+PALLAVI-HALABAVI@users.noreply.github.com>
2024-08-29 17:52:19 +0530

    Update and rename atm_database (1).sql to atm_database .sql

--- atm_database .sql
+++ atm_database .sql
@@ -1,27 +1,18 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 05, 2023 at 05:43 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `atm database`
 --
 
--- --------------------------------------------------------
 
 --
 -- Table structure for table `account`
@@ -397,6 +388,3 @@ ALTER TABLE `savings`
   ADD CONSTRAINT `savings_ibfk_1` FOREIGN KEY (`Acc_no`) REFERENCES `account` (`Acc_no`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
